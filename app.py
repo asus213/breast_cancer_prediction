1086b1af4406d0f23504a31e5682e31ae055baa9
asus213 <asusmahadev@gmail.com>
2026-02-26 19:57:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,28 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
 model = pickle.load(open('breast_cancer_model.pkl', 'rb'))
+df = pd.read_csv("breast-cancer.csv")
+
+X = df.drop(["id", "diagnosis"], axis=1)
+y = df["diagnosis"]
+y = y.map({"M": 1, "B": 0})
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+st.write("Model Accuracy:", round(accuracy*100, 2), "%")
 
 st.title("Breast Cancer Prediction App")
 
